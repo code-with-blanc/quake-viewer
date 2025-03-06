6cf1e4f192f951a16670154d49c6d7122411fad7
Blanc <pedro.arabe@doglife.com.br>
2025-03-06 11:56:44 -0300

    fix clicks on overlay

--- src/components/scene-overlay/sceneOverlay.js
+++ src/components/scene-overlay/sceneOverlay.js
@@ -10,14 +10,14 @@ export default function SceneOverlay() {
   return (
     <div className='scene-overlay-container'>
         <div className='scene-overlay-btns'>
-          <IconButton
-            icon="linkedin" alt="Reach me on LinkedIn"
-            onClick={() => window.open(LINKEDIN_PROFILE, '_blank')}
-          />
-          <IconButton 
-            icon="github" alt="Source code"
-            onClick={() => { window.open(GITHUB, '_blank') }}
-          />
+            <IconButton
+              icon="linkedin" alt="Reach me on LinkedIn"
+              onClick={() => window.open(LINKEDIN_PROFILE, '_blank')}
+            />
+            <IconButton 
+              icon="github" alt="Source code"
+              onClick={() => { window.open(GITHUB, '_blank') }}
+            />
         </div>
         <div className='scene-overlay-timeline'>
             <Timeline />
